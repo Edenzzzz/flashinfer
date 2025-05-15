823d9202c026e2c5bd18a56a7a636d677de2ee04
Edenzzzz <wtan45@wisc.edu>
2025-05-15 01:03:28 +0000

    fix

--- benchmarks/bench_mixed_attention.py
+++ benchmarks/bench_mixed_attention.py
@@ -1,12 +1,12 @@
 import logging
 
+logging.disable(logging.WARNING)
+
 import numpy as np
 import torch
 from triton.testing import do_bench
 
 import flashinfer
-
-logging.disable(logging.WARNING)
 
 
 def run_bench(
@@ -86,9 +86,12 @@ def run_bench(
             workspace_buffer,
             kv_layout=kv_layout,
         )
+        kv_indices_d = torch.arange(
+            0, d_kv_indptr[-1], device="cuda:0", dtype=torch.int32
+        )
         wrapper_pod.plan(
-            d_q_indptr.to(device),
             d_kv_indptr.to(device),
+            kv_indices_d,
             last_page_len=last_page_len_d,
             num_qo_heads=num_qo_heads,
             num_kv_heads=num_kv_heads,
@@ -106,7 +109,7 @@ def run_bench(
                 kv_d,
                 causal_p=causal,
                 causal_d=causal,
-            )
+            ),
         )
 
     wrapper = flashinfer.BatchAttention(kv_layout=kv_layout)
@@ -126,23 +129,23 @@ def run_bench(
     )
     ms = do_bench(lambda: wrapper.run(q, kv_data))
 
-    print(f"Elapsed time (old scheduler): {ms_old:.2f} ms")
-    print(f"Elapsed time (mixed scheduler): {ms:.2f} ms")
+    print(f"Elapsed time (old scheduler): {ms_old:.3f} ms")
+    print(f"Elapsed time (mixed scheduler): {ms:.3f} ms")
     if page_block_size == 1 and len(p_qo_lens) > 0:
-        print(f"Elapsed time (POD Attention): {ms_pod:.2f} ms")
+        print(f"Elapsed time (POD Attention): {ms_pod:.3f} ms")
     total_bytes = (
         q.numel() * q.element_size() + kv_data.numel() * kv_data.element_size()
     )
-    print(f"Loading memory size (MB): {total_bytes / (1024**2):.2f} MB")
+    print(f"Loading memory size (MB): {total_bytes / (1024**2):.3f} MB")
 
     bandwidth_old_gb_s = total_bytes / (ms_old * 1e-3) / (1024**3)
     bandwidth_mixed_gb_s = total_bytes / (ms * 1e-3) / (1024**3)
 
-    print(f"Memory bandwidth (old scheduler): {bandwidth_old_gb_s:.2f} GB/s")
-    print(f"Memory bandwidth (mixed scheduler): {bandwidth_mixed_gb_s:.2f} GB/s")
+    print(f"Memory bandwidth (old scheduler): {bandwidth_old_gb_s:.3f} GB/s")
+    print(f"Memory bandwidth (mixed scheduler): {bandwidth_mixed_gb_s:.3f} GB/s")
     if page_block_size == 1 and len(p_qo_lens) > 0:
         bandwidth_pod_gb_s = total_bytes / (ms_pod * 1e-3) / (1024**3)
-        print(f"Memory bandwidth (POD Attention): {bandwidth_pod_gb_s:.2f} GB/s")
+        print(f"Memory bandwidth (POD Attention): {bandwidth_pod_gb_s:.3f} GB/s")
     print()
 
 
@@ -150,12 +153,12 @@ if __name__ == "__main__":
     np.random.seed(42)
     torch.random.manual_seed(42)
 
-    # seq_len_configs = [
-    #     [(600, 1)] * 122 + [(10000, 17)] * 8,
-    #     [(10000, 1)] * 128,
-    #     [(400, 1)] * 242 + [(8192, 17)] * 16,
-    #     [(8192, 1)] * 256,
-    # ]
+    seq_len_configs = [
+        [(600, 1)] * 122 + [(10000, 17)] * 8,
+        [(10000, 1)] * 128,
+        [(400, 1)] * 242 + [(8192, 17)] * 16,
+        [(8192, 1)] * 256,
+    ]
 
     # POD Attention
     d_q_len_configs = [[1] * 122, [1] * 128, [1] * 242, [1] * 256]
@@ -223,6 +226,7 @@ if __name__ == "__main__":
     num_kv_heads = 4
     num_qo_heads = 28
     head_dim = 128
+    torch.cuda.cudart().cudaProfilerStart()
 
     for idx, (p_q_lens, p_kv_lens, d_q_len, d_kv_len) in enumerate(
         zip(p_q_configs, p_kv_configs, d_q_len_configs, d_kv_len_configs)
@@ -241,3 +245,4 @@ if __name__ == "__main__":
             device=0,
             causal=True,
         )
+    torch.cuda.cudart().cudaProfilerStop()
