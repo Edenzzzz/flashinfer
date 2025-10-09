fc4acce7571e5980e6224865e5d18a1fba01c6f6
Edenzzzz <wtan45@wisc.edu>
2025-10-09 20:33:39 +0000

    rm random

--- benchmarks/bench_mixed_attention.py
+++ benchmarks/bench_mixed_attention.py
@@ -174,60 +174,60 @@ if __name__ == "__main__":
     p_kv_configs = [[10000] * 1, [10000], [8192] * 1, []]
 
     # construct random length testcases
-    for _ in range(1):
-        bsz = 256
-        stride = 16
-        sparsity = 0.05
+    # for _ in range(1):
+    #     bsz = 256
+    #     stride = 16
+    #     sparsity = 0.05
 
-        full_kv_len = np.random.randint(1000, 8192, size=bsz)
-        p_q_lens = []
-        p_kv_lens = []
-        d_q_lens = []
-        d_kv_lens = []
-        for i in range(bsz):
-            if i % stride == 0:
-                kv_len = full_kv_len[i]
-                qo_len = stride + 1
-                p_q_lens.append(qo_len)
-                p_kv_lens.append(kv_len)
-            else:
-                kv_len = int(full_kv_len[i] * sparsity)
-                qo_len = 1
-                d_q_lens.append(qo_len)
-                d_kv_lens.append(kv_len)
+    #     full_kv_len = np.random.randint(1000, 8192, size=bsz)
+    #     p_q_lens = []
+    #     p_kv_lens = []
+    #     d_q_lens = []
+    #     d_kv_lens = []
+    #     for i in range(bsz):
+    #         if i % stride == 0:
+    #             kv_len = full_kv_len[i]
+    #             qo_len = stride + 1
+    #             p_q_lens.append(qo_len)
+    #             p_kv_lens.append(kv_len)
+    #         else:
+    #             kv_len = int(full_kv_len[i] * sparsity)
+    #             qo_len = 1
+    #             d_q_lens.append(qo_len)
+    #             d_kv_lens.append(kv_len)
 
-        p_q_configs.append(p_q_lens)
-        p_kv_configs.append(p_kv_lens)
-        d_q_len_configs.append(d_q_lens)
-        d_kv_len_configs.append(d_kv_lens)
+    #     p_q_configs.append(p_q_lens)
+    #     p_kv_configs.append(p_kv_lens)
+    #     d_q_len_configs.append(d_q_lens)
+    #     d_kv_len_configs.append(d_kv_lens)
 
-    for _ in range(1):
-        bsz = 128
-        stride = 16
-        sparsity = 0.05
+    # for _ in range(1):
+    #     bsz = 128
+    #     stride = 16
+    #     sparsity = 0.05
 
-        full_kv_len = np.random.randint(2000, 16000, size=bsz)
-        p_q_lens = []
-        p_kv_lens = []
-        d_q_lens = []
-        d_kv_lens = []
+    #     full_kv_len = np.random.randint(2000, 16000, size=bsz)
+    #     p_q_lens = []
+    #     p_kv_lens = []
+    #     d_q_lens = []
+    #     d_kv_lens = []
 
-        for i in range(bsz):
-            if i % stride == 0:
-                kv_len = full_kv_len[i]
-                qo_len = stride + 1
-                p_q_lens.append(qo_len)
-                p_kv_lens.append(kv_len)
-            else:
-                kv_len = int(full_kv_len[i] * sparsity)
-                qo_len = 1
-                d_q_lens.append(qo_len)
-                d_kv_lens.append(kv_len)
+    #     for i in range(bsz):
+    #         if i % stride == 0:
+    #             kv_len = full_kv_len[i]
+    #             qo_len = stride + 1
+    #             p_q_lens.append(qo_len)
+    #             p_kv_lens.append(kv_len)
+    #         else:
+    #             kv_len = int(full_kv_len[i] * sparsity)
+    #             qo_len = 1
+    #             d_q_lens.append(qo_len)
+    #             d_kv_lens.append(kv_len)
 
-        p_q_configs.append(p_q_lens)
-        p_kv_configs.append(p_kv_lens)
-        d_q_len_configs.append(d_q_lens)
-        d_kv_len_configs.append(d_kv_lens)
+    #     p_q_configs.append(p_q_lens)
+    #     p_kv_configs.append(p_kv_lens)
+    #     d_q_len_configs.append(d_q_lens)
+    #     d_kv_len_configs.append(d_kv_lens)
 
     page_block_size = 1
     num_kv_heads = 4
